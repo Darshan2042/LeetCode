cbbd1cea1dd59ee98004b885411d5dcda6c2a3cb
Darshan Pawar <150545577+Darshan2042@users.noreply.github.com>
2026-07-30 12:00:58 +0530

    Time: 0 ms (100%), Space: 43 MB (89%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -2,17 +2,8 @@ class Solution {
     public int minimumPushes(String word) {
         int n = word.length();
         int count = 0;
-        if(n <= 8){
-            count = n;
-        }
-        else if(n <= 16){
-            count = 8 + (n-8)*2;
-        }
-        else if(n <= 24){
-            count = 24 + (n-16)*3;
-        }
-        else{
-            count = 48 +(n-24)*4;
+        for(int i=0 ; i<n ; i++){
+            count += i/8+1;
         }
         return count;
     }
