d7913f997e362d7ea794ab4a233a70661e2c5a54
Darshan Pawar <150545577+Darshan2042@users.noreply.github.com>
2026-08-03 10:19:36 +0530

    Time: 7 ms (88.35%), Space: 85.4 MB (76.7%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -4,12 +4,11 @@ class Solution {
         int[] dp = new int[n+1];
         for(int i=n-1; i>=0 ; i--){
             int sum = 0;
-            int best = Integer.MIN_VALUE;
-            for(int k=0; k<3 && i+k<n; k++){
+            int best = Integer.MIN_VALUE; // Start with the smallest value because we want the maximum answer.
+            for(int k=0; k<3 && i+k<n; k++){// Try taking 1, 2, or 3 stones i + k < n prevents going outside the array.
                 sum += stoneValue[i+k];
-                best = Math.max(best,sum-dp[i+k+1]);
+                best = Math.max(best,sum-dp[i+k+1]);// Choose the move that gives the maximum score difference.
             }
-
             dp[i] = best;
         }
 
