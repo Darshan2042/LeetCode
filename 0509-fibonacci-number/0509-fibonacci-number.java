9d713e2e00b4d7afed6a3179ca26f12e9c45e158
Darshan Pawar <150545577+Darshan2042@users.noreply.github.com>
2026-08-13 17:44:42 +0530

    Time: 0 ms (100%), Space: 41.9 MB (85.98%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        int a = 0;
+        int b = 1;
+        for(int i=0; i<n; i++){
+            int t = a+b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+}
