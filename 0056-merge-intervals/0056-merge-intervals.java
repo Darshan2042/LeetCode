4e6bcaa4715d1b1c112ab332e25fd12dfe88de98
Darshan Pawar <150545577+Darshan2042@users.noreply.github.com>
2026-08-23 18:04:49 +0530

    Time: 7 ms (99.06%), Space: 49.3 MB (14.27%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,12 +1,7 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if(intervals.length <= 1){
-            return intervals;
-        }
-
-        Arrays.sort(intervals,(a,b) -> a[0] - b[0]);
-        List<int []> list = new ArrayList<>();
-
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        List<int[]> list = new ArrayList<>();
         int[] st = intervals[0];
         list.add(st);
 
@@ -20,6 +15,5 @@ class Solution {
             }
         }
         return list.toArray(new int[list.size()][]);
-
     }
 }
