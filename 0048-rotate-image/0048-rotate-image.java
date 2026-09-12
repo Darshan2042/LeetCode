3991b6e6d727ff480cb21794904276330a32b530
Darshan Pawar <150545577+Darshan2042@users.noreply.github.com>
2026-09-12 11:45:01 +0530

    Time: 0 ms (100%), Space: 43.7 MB (52.78%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,13 +1,14 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        for(int i=0;i<matrix.length-1;i++){
-            for(int j=i+1;j<matrix[i].length;j++){
+        for(int i=0; i<matrix.length; i++){
+            for(int j=i+1; j<matrix.length; j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
-        for(int i=0;i<matrix.length;i++){
+
+        for(int i=0; i<matrix.length; i++){
             reverse(matrix[i]);
         }
     }
@@ -23,5 +24,4 @@ class Solution {
             right--;
         }
     }
-
 }
